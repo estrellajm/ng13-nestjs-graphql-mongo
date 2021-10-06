9aa58d0bd595ca2d4f5d20452b07667db2151657
Jose Estrella <jose.estrella@imploie.com>
2021-10-06 06:53:46 -0600

    added cats sample

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -4,18 +4,23 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from './main/user/user.module';
+import { CatsModule } from './main/cats/cats.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/users', {
       connectionName: 'users'
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/cats', {
+      connectionName: 'cats'
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       playground: true,
       debug: false
     }),
-    UserModule
+    UserModule,
+    CatsModule
   ],
   providers: [AppService, AppResolver]
 })
